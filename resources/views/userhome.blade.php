823e214528924a1440067a04eaddf29378ec5126
Shubham <sv2621228@gmail.com>
2021-06-14 08:21:33 +0530

    Inital Project With Password
    1. Login
    2. Register
    3. Category

---
+++ resources/views/userhome.blade.php
@@ -0,0 +1,363 @@
+@extends('user.userdashboard')
+@section('user')
+
+
+<!-- main-slider -->
+<ul id="demo1">
+			<li>
+				<img src="{{ asset('usertemplate/images/1a.png') }}" alt="" />
+				<!--Slider Description example-->
+				<div class="slide-desc">
+					<h3>All Men's Products Here</h3>
+				</div>
+			</li>
+			<li>
+				<img src="{{ asset('usertemplate/images/1b.png') }}" alt="" />
+				  <div class="slide-desc">
+					<h3>All Women's Products Here</h3>
+				</div>
+			</li>
+			
+			<li>
+				<img src="{{ asset('usertemplate/images/1c.png') }}" alt="" />
+				<div class="slide-desc">
+					<h3>All Kid's Products Here</h3>
+				</div>
+			</li>
+		</ul>
+	<!-- //main-slider -->
+	<!-- //top-header and slider -->
+	
+	
+<!--banner-bottom-->
+				<div class="ban-bottom-w3l">
+					<div class="container">
+					<div class="col-md-6 ban-bottom3">
+							<div class="ban-top">
+								<img src="{{ asset('usertemplate/images/1a.png') }}" class="img-responsive" alt=""/>
+								
+							</div>
+							<div class="ban-img">
+								<div class=" ban-bottom1">
+									<div class="ban-top">
+										<img src="{{ asset('usertemplate/images/p2.jpg') }}" class="img-responsive" alt=""/>
+										
+									</div>
+								</div>
+								<div class="ban-bottom2">
+									<div class="ban-top">
+										<img src="{{ asset('usertemplate/images/p2.png') }}" class="img-responsive" alt=""/>
+										
+									</div>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+						</div>
+						<div class="col-md-6 ban-bottom">
+							<div class="ban-top">
+								<img src="{{ asset('usertemplate/images/111.jpg') }}" class="img-responsive" alt=""/>
+								
+								
+							</div>
+						</div>
+						
+						<div class="clearfix"></div>
+					</div>
+				</div>
+<!--banner-bottom-->
+<!--brands-->
+	<div class="brands">
+		<div class="container">
+		<h3>Brand Store</h3>
+			<div class="brands-agile">
+				<div class="col-md-2 w3layouts-brand">
+					<div class="brands-w3l">
+						<p><a href="#">Levi's</a></p>
+					</div>
+				</div>
+				<div class="col-md-2 w3layouts-brand">
+					<div class="brands-w3l">
+						<p><a href="#">Pepe Jeans</a></p>
+					</div>
+				</div>
+				<div class="col-md-2 w3layouts-brand">
+					<div class="brands-w3l">
+						<p><a href="#">Tommy Hilfiger</a></p>
+					</div>
+				</div>
+				
+				<div class="col-md-2 w3layouts-brand">
+					<div class="brands-w3l">
+						<p><a href="#">5. Lee</a></p>
+					</div>
+				</div>
+				<div class="col-md-2 w3layouts-brand">
+					<div class="brands-w3l">
+						<p><a href="#">Wrangler</a></p>
+					</div>
+				</div>
+				<div class="col-md-2 w3layouts-brand">
+					<div class="brands-w3l">
+						<p><a href="#">Adidas</a></p>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="brands-agile-1">
+				<div class="col-md-2 w3layouts-brand">
+					<div class="brands-w3l">
+						<p><a href="#">BIBA</a></p>
+					</div>
+				</div>
+				<div class="col-md-2 w3layouts-brand">
+					<div class="brands-w3l">
+						<p><a href="#">FABINDIA</a></p>
+					</div>
+				</div>
+				<div class="col-md-2 w3layouts-brand">
+					<div class="brands-w3l">
+						<p><a href="#">AURELIA</a></p>
+					</div>
+				</div>
+				
+				<div class="col-md-2 w3layouts-brand">
+					<div class="brands-w3l">
+						<p><a href="#">WESTSIDE</a></p>
+					</div>
+				</div>
+				<div class="col-md-2 w3layouts-brand">
+					<div class="brands-w3l">
+						<p><a href="#">W for Women</a></p>
+					</div>
+				</div>
+				<div class="col-md-2 w3layouts-brand">
+					<div class="brands-w3l">
+						<p><a href="#">AND</a></p>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="brands-agile-2">
+				<div class="col-md-2 w3layouts-brand">
+					<div class="brands-w3l">
+						<p><a href="#">Gini & Jony</a></p>
+					</div>
+				</div>
+				<div class="col-md-2 w3layouts-brand">
+					<div class="brands-w3l">
+						<p><a href="#">612 League</a></p>
+					</div>
+				</div>
+				<div class="col-md-2 w3layouts-brand">
+					<div class="brands-w3l">
+						<p><a href="#">Little Kangaroos</a></p>
+					</div>
+				</div>
+				
+				<div class="col-md-2 w3layouts-brand">
+					<div class="brands-w3l">
+						<p><a href="#">Nauti Nati</a></p>
+					</div>
+				</div>
+				<div class="col-md-2 w3layouts-brand">
+					<div class="brands-w3l">
+						<p><a href="#">Babyhug</a></p>
+					</div>
+				</div>
+				<div class="col-md-2 w3layouts-brand">
+					<div class="brands-w3l">
+						<p><a href="#">Ajio</a></p>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>	
+<!--//brands-->
+<!-- new -->
+	<div class="newproducts-w3agile">
+		<div class="container">
+			<h3>New offers</h3>
+				<div class="agile_top_brands_grids">
+					<div class="col-md-3 top_brand_left-1">
+						<div class="hover14 column">
+							<div class="agile_top_brand_left_grid">
+								<div class="agile_top_brand_left_grid_pos">
+									<img src="{{ asset('usertemplate/images/offer.png') }}" alt=" " class="img-responsive">
+								</div>
+								<div class="agile_top_brand_left_grid1">
+									<figure>
+										<div class="snipcart-item block">
+											<div class="snipcart-thumb">
+												<a href="#"><img title=" " alt=" " src="{{ asset('usertemplate/images/2a.png')}}"></a>		
+												<p>Kid's Wear</p>
+												<div class="stars">
+													<i class="fa fa-star blue-star" aria-hidden="true"></i>
+													<i class="fa fa-star blue-star" aria-hidden="true"></i>
+													<i class="fa fa-star blue-star" aria-hidden="true"></i>
+													<i class="fa fa-star blue-star" aria-hidden="true"></i>
+													<i class="fa fa-star gray-star" aria-hidden="true"></i>
+												</div>
+													<h4>$ 30 <span>$ 60</span></h4>
+											</div>
+											<div class="snipcart-details top_brand_home_details">
+												<form action="#" method="post">
+													<fieldset>
+														<input type="hidden" name="cmd" value="_cart">
+														<input type="hidden" name="add" value="1">
+														<input type="hidden" name="business" value=" ">
+														<input type="hidden" name="item_name" value="Fortune Sunflower Oil">
+														<input type="hidden" name="amount" value="35.99">
+														<input type="hidden" name="discount_amount" value="1.00">
+														<input type="hidden" name="currency_code" value="USD">
+														<input type="hidden" name="return" value=" ">
+														<input type="hidden" name="cancel_return" value=" ">
+														<input type="submit" name="submit" value="View More" class="button">
+													</fieldset>
+												</form>
+											</div>
+										</div>
+									</figure>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 top_brand_left-1">
+						<div class="hover14 column">
+							<div class="agile_top_brand_left_grid">
+								<div class="agile_top_brand_left_grid_pos">
+									<img src="{{ asset('usertemplate/images/offer.png') }}" alt=" " class="img-responsive">
+								</div>
+								<div class="agile_top_brand_left_grid1">
+									<figure>
+										<div class="snipcart-item block">
+											<div class="snipcart-thumb">
+												<a href="#"><img title=" " alt=" " src="{{ asset('usertemplate/images/2b.png')}}"></a>		
+												<p>Men's Clothes</p>
+												<div class="stars">
+													<i class="fa fa-star blue-star" aria-hidden="true"></i>
+													<i class="fa fa-star blue-star" aria-hidden="true"></i>
+													<i class="fa fa-star blue-star" aria-hidden="true"></i>
+													<i class="fa fa-star blue-star" aria-hidden="true"></i>
+													<i class="fa fa-star gray-star" aria-hidden="true"></i>
+												</div>
+													<h4>$ 25 <span>$ 40</span></h4>
+											</div>
+											<div class="snipcart-details top_brand_home_details">
+												<form action="#" method="post">
+													<fieldset>
+														<input type="hidden" name="cmd" value="_cart">
+															<input type="hidden" name="add" value="1">
+															<input type="hidden" name="business" value=" ">
+															<input type="hidden" name="item_name" value="basmati rise">
+															<input type="hidden" name="amount" value="30.99">
+															<input type="hidden" name="discount_amount" value="1.00">
+															<input type="hidden" name="currency_code" value="USD">
+															<input type="hidden" name="return" value=" ">
+															<input type="hidden" name="cancel_return" value=" ">
+															<input type="submit" name="submit" value="View More" class="button">
+													</fieldset>
+												</form>
+											</div>
+										</div>
+									</figure>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 top_brand_left-1">
+						<div class="hover14 column">
+							<div class="agile_top_brand_left_grid">
+								<div class="agile_top_brand_left_grid_pos">
+									<img src="{{ asset('usertemplate/images/offer.png')}}" alt=" " class="img-responsive">
+								</div>
+								<div class="agile_top_brand_left_grid_pos">
+									<img src="{{ asset('usertemplate/images/offer.png')}}" alt=" " class="img-responsive">
+								</div>
+								<div class="agile_top_brand_left_grid1">
+									<figure>
+										<div class="snipcart-item block">
+											<div class="snipcart-thumb">
+												<a href="#"><img src="{{ asset('usertemplate/images/2c.png')}}" alt=" " class="img-responsive"></a>
+												<p>Women's Clothes</p>
+												<div class="stars">
+													<i class="fa fa-star blue-star" aria-hidden="true"></i>
+													<i class="fa fa-star blue-star" aria-hidden="true"></i>
+													<i class="fa fa-star blue-star" aria-hidden="true"></i>
+													<i class="fa fa-star blue-star" aria-hidden="true"></i>
+													<i class="fa fa-star gray-star" aria-hidden="true"></i>
+												</div>
+													<h4>$ 20 <span>$ 30</span></h4>
+											</div>
+											<div class="snipcart-details top_brand_home_details">
+												<form action="#" method="post">
+													<fieldset>
+														<input type="hidden" name="cmd" value="_cart">
+														<input type="hidden" name="add" value="1">
+														<input type="hidden" name="business" value=" ">
+														<input type="hidden" name="item_name" value="Pepsi soft drink">
+														<input type="hidden" name="amount" value="80.00">
+														<input type="hidden" name="discount_amount" value="1.00">
+														<input type="hidden" name="currency_code" value="USD">
+														<input type="hidden" name="return" value=" ">
+														<input type="hidden" name="cancel_return" value=" ">
+														<input type="submit" name="submit" value="View More" class="button">
+													</fieldset>
+												</form>
+											</div>
+										</div>
+									</figure>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 top_brand_left-1">
+						<div class="hover14 column">
+							<div class="agile_top_brand_left_grid">
+								<div class="agile_top_brand_left_grid_pos">
+									<img src="{{ asset('usertemplate/images/offer.png')}}" alt=" " class="img-responsive">
+								</div>
+								<div class="agile_top_brand_left_grid1">
+									<figure>
+										<div class="snipcart-item block">
+											<div class="snipcart-thumb">
+												<a href="#"><img title=" " alt=" " src="{{ asset('usertemplate/images/2d.png')}}"></a>		
+												<p>Watch</p>
+												<div class="stars">
+													<i class="fa fa-star blue-star" aria-hidden="true"></i>
+													<i class="fa fa-star blue-star" aria-hidden="true"></i>
+													<i class="fa fa-star blue-star" aria-hidden="true"></i>
+													<i class="fa fa-star blue-star" aria-hidden="true"></i>
+													<i class="fa fa-star gray-star" aria-hidden="true"></i>
+												</div>
+													<h4>$ 40 <span>$ 60</span></h4>
+											</div>
+											<div class="snipcart-details top_brand_home_details">
+												<form action="#" method="post">
+													<fieldset>
+														<input type="hidden" name="cmd" value="_cart">
+														<input type="hidden" name="add" value="1">
+														<input type="hidden" name="business" value=" ">
+														<input type="hidden" name="item_name" value="Fortune Sunflower Oil">
+														<input type="hidden" name="amount" value="35.99">
+														<input type="hidden" name="discount_amount" value="1.00">
+														<input type="hidden" name="currency_code" value="USD">
+														<input type="hidden" name="return" value=" ">
+														<input type="hidden" name="cancel_return" value=" ">
+														<input type="submit" name="submit" value="View More" class="button">
+													</fieldset>
+												</form>
+											</div>
+										</div>
+									</figure>
+								</div>
+							</div>
+						</div>
+					</div>
+						<div class="clearfix"> </div>
+				</div>
+		</div>
+	</div>
+<!-- //new -->
+
+@endsection
